c67bfcc394c4a110be3a5b4882549e62e9422699
finan <finanmeharenakidane@gmail.com>
2023-03-04 07:40:20 +0400

    added on/off images, function to toggle between on/off functions

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const lightButton=document.querySelector(".light")
-
-const mainElement = document.querySelector(".main")
+const lightButton = document.querySelector(".light")
+const hero = document.querySelector(".hero")
+const onBulb = document.querySelector(".on-img")
+const maineElement = document.querySelector(".main")
 
 
 lightButton.addEventListener('click', ()=>{
-    mainElement.classList.toggle("light-change") 
+    hero.classList.toggle('light-change')
+    onBulb.classList.toggle("switch-on")
+    onBulb.classList.toggle("shake")
+    maineElement.classList.toggle("change-body-color")
 })
